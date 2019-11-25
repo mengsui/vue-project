54cd5b39837ce5f3e59cc858e69c11566602f63f
比目鱼(王悦) <bimuyu.wangy@purplechain.cn>
2019-11-25 15:50:31 +0800

    修改路由守卫

--- antd-demo-vue/src/permission.js
+++ antd-demo-vue/src/permission.js
@@ -53,9 +53,9 @@ router.beforeEach((to, from, next) => {
         /**
           * 这里应该使用 next() 如果后台没有处理token超时返回的解构问题，前端强制跳转的登陆页面
         */
-        // next();
-        Vue.ls.clear();
-        next('/manage/login');
+        next();
+        // Vue.ls.clear();
+        // next('/manage/login');
       }
     };
   } else {
